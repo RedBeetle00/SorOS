e072c42de353944fc63b93a83853c47401e814b9
redbeetle00 <redjuks@gmail.com>
2025-11-25 21:13:56 +0300

    yes

--- boot/boot.asm
+++ boot/boot.asm
@@ -2,24 +2,41 @@ bits 16
 org 0x7c00
 
 start:
+	call print_privatstvie
+	jmp $
+
+print_privatstvie:
+	; screen clear
+	mov ah, 0x06
+	mov al, 0x00
+	mov bh, 0x07
+	mov cx, 0x0000
+	mov dx, 0x184f
+	int 0x10
+
+	; videomode
 	mov ah, 0x00
 	mov al, 0x0a
 	int 0x10
 
+	; cursor pos
 	mov ah, 0x02
 	mov dh, 0
 	mov dl, 0
 	mov bh, 0
 	int 0x10
 
-	mov ah, 0x09
-	mov al, 65
-	mov bh, 0
-	mov bl, 4
-	mov cx, 4
+	;symbol write
+	mov ax, 0x1300
+	mov bx, 0x0004
+	mov cx, 12
+	mov dx, 0x0820
+	mov bp, msg
 	int 0x10
 
-	jmp $
+	ret
+	
+msg db "Hello, man",0
 
 times 510-($-$$) db 0
 
